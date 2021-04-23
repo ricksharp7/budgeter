77dcbe3f986200350c1a888f1e01ce61d377ab83
Rick Sharp <rick.sharp@interfolio.com>
2021-04-23 11:19:09 +0100

    Artefacts, plus breadcrumbs to know if it's successful.

---
+++ database/migrations/2021_04_23_123456_create_dummy_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDummyTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('codebuild', function (Blueprint $table) {
+            $table->id();
+            $table->string('success', 128);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('codebuild');
+    }
+}
--- resources/js/Pages/WelcomeComponent.vue
+++ resources/js/Pages/WelcomeComponent.vue
@@ -2,6 +2,7 @@
   <div title="Welcome">
     <h1>Welcome</h1>
     <p>Hello, welcome to your first Inertia app!</p>
+    <p>This was deployed with CodeBuild.</p>
   </div>
 </template>
 
